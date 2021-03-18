519644514cae0868ac55aed59e0fad86cb860a58
Arkadiusz Karpinski <arekkarpinski2@gmail.com>
2021-03-18 12:06:24 +0100

    updating scoro_client

--- app/objects/scoro_client.rb
+++ app/objects/scoro_client.rb
@@ -1,15 +1,52 @@
 class ScoroClient
   def get_contacts_page(page)
-
     scoro_response = HTTP.post("https://arek.scoro.com/api/v2/contacts/list", json: { apiKey: ENV["SCORO_API_KEY"], company_account_id: "arek", per_page: 5, page: page })
 
-    scoro_response_json = scoro_response.body.to_s
-    scoro_result = MultiJson.load(scoro_response_json)
-    scoro_result["data"]
+    success = scoro_response.status == 200
+    if success 
+      scoro_response_json = scoro_response.body.to_s
+      scoro_result = MultiJson.load(scoro_response_json)
+      { success: true, contacts: scoro_result["data"] }
+    else
+      { success: false, http_status: scoro_response.status }
+    end    
   end
 
   def create_contact(contact)
+    scoro_response = HTTP.post("https://arek.scoro.com/api/v2/contacts/modify", json: { "apiKey": ENV["SCORO_API_KEY"], "company_account_id": "arek", "request": contact })
+    
+    success = scoro_response.status == 200
+    
+    if success 
+      scoro_response_json = scoro_response.body.to_s
+      scoro_result = MultiJson.load(scoro_response_json)
+      { success: true, contact: scoro_result["data"] }
+    else
+      { success: false, http_status: scoro_response.status }
+    end
+  end
 
-    HTTP.post("https://arek.scoro.com/api/v2/contacts/modify", json: { "apiKey": ENV["SCORO_API_KEY"], "company_account_id": "arek", "request": contact })
+  def delete_contact(id)
+    scoro_response = HTTP.post("https://arek.scoro.com/api/v2/contacts/delete/#{id}", json: { "apiKey": ENV["SCORO_API_KEY"], "company_account_id": "arek" })
+    
+    success = scoro_response.status == 200
+    if success
+      { success: true }
+    else
+      { success: false, http_status: scoro_response.status }
+    end
+  end
+
+  def show_contact(id)
+    scoro_response = HTTP.post("https://arek.scoro.com/api/v2/contacts/view/#{id}", json: { "apiKey": ENV["SCORO_API_KEY"], "company_account_id": "arek" })
+    
+    success = scoro_response.status == 200
+    if success
+      scoro_response_json = scoro_response.body.to_s
+      scoro_result = MultiJson.load(scoro_response_json)
+      { success: true, contact: scoro_result["data"] }   
+    else
+      { success: false, http_status: scoro_response.status }
+    end
   end
 end
--- spec/unit/scoro_client_spec.rb
+++ spec/unit/scoro_client_spec.rb
@@ -5,12 +5,13 @@ describe ScoroClient do
     scoro_client = ScoroClient.new
 
     result = nil
-    VCR.use_cassette("contact_list") do
+    VCR.use_cassette("contact_list", :match_requests_on => [:body]) do
       result = scoro_client.get_contacts_page(2)
     end
 
-    expect(result.first["name"]).to eq "Toby"
-    expect(result.size).to eq 5
+    expect(result[:success]).to be true
+    expect(result[:contacts].first["name"]).to eq "Toby"
+    expect(result[:contacts].size).to eq 5
   end
 
   it "creates contact" do        
@@ -25,22 +26,42 @@ describe ScoroClient do
       "website": nil
      }
 
+    result = nil
     VCR.use_cassette('create_contact') do
-      scoro_client.create_contact(contact)
+      result = scoro_client.create_contact(contact)
     end
 
+    expect(result[:success]).to be true
+    expect(result[:contact]["name"]).to eq "Olaf"
     expect(WebMock).to have_requested(:post, "https://arek.scoro.com/api/v2/contacts/modify")
       .with(body: '{"apiKey":"ScoroAPI_a4e5e6ad85ecdcc","company_account_id":"arek","request":{"type":"person","name":"Olaf","lastname":"Kowal","mobile":"+728555777","email":"olaf@gmail.com","phone":null,"website":null}}')    
   end
 
-#   it "deletes contact" do
-#     scoro_client = ScoroClient.new
-#     scoro_client.delete_client(1)
+  it "deletes contact" do
+    scoro_client = ScoroClient.new
+    id = 2
     
-#     VCR.use_cassette('create_contact') do
-#       scoro_client.delete_client(1)
-#     end
+    result = nil
+    VCR.use_cassette('delete_contact') do
+      result = scoro_client.delete_contact(id)
+    end
 
-#   end
+    expect(result[:success]).to be true
+    expect(WebMock).to have_requested(:post, "https://arek.scoro.com/api/v2/contacts/delete/2") 
+  end
+  
+  it "shows contact" do
+    scoro_client = ScoroClient.new
+    id = 11
+    
+    result = nil
+    VCR.use_cassette('show_contact') do
+      result = scoro_client.show_contact(id)
+    end
+    
+    expect(result[:success]).to be true
+    expect(result[:contact]["name"]).to eq "Toby"
+    expect(WebMock).to have_requested(:post, "https://arek.scoro.com/api/v2/contacts/view/11") 
+  end
   
 end
